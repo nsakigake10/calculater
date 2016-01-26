9e2d859fba2842ee0781daa38b37ec90fbfcdde2
長島魁杜 <liverpool0715sas@yahoo.co.jp>
2016-01-26 17:27:56 +0900

    second commit.

--- dentaku.php
+++ dentaku.php
@@ -6,25 +6,29 @@
 
     */
     
-    $kazu_1 = "0";
-    $kazu_2 = "0";
-    $kazu_3 = "0";
-    $ope    = "+";
+    $num_1    = "";
+    $num_2    = "";
+    $num_3    = "";
+    $operator = "+";
     
-    if(isset($_POST['kazu_1']))$kazu_1  = $_POST['kazu_1'];
-    if(isset($_POST['kazu_2']))$kazu_2  = $_POST['kazu_2'];
-    if(isset($_POST['operator'])){
-        $ope       = $_POST['operator'];
+    
+    if(isset($_POST['num_1']))      $num_1    = $_POST['num_1'];
+    if(isset($_POST['num_2']))      $num_2    = $_POST['num_2'];
+    if(isset($_POST['operator']))   $operator = $_POST['operator'];
+    
+    
+    
+    
+    if(ctype_digit($num_1) === false){
+        print("Error : num_1 Please input number\n");
     }
     
-    
-    if(ctype_digit($kazu_1) == false){
-        print("Error : kazu_1 Please input number\n");
+    if(ctype_digit($num_2) === false){
+        print("Error : num_2 Please input number\n");
     }
+
     
-    if(ctype_digit($kazu_2) == false){
-        print("Error : kazu_2 Please input number\n");
-    }
+    //==は無理やり等価を成り立たせる可能性がある
 
 
     
@@ -32,18 +36,24 @@
     //GET = データがURLで引き渡される
     //POST = データがURLで引き渡されない
     
+    if((ctype_digit($num_1))&&(ctype_digit($num_2))){
     
+    if($operator == "+") $num_3 = $num_1 + $num_2;
+    if($operator == "/") {
+        if($num_2 == 0){
+            print("Error : not capable of being computed\n");
+        } else {
+            $num_3 = $num_1 / $num_2;
+        }
+    }
+    if($operator == "-") $num_3 = $num_1 - $num_2;
+    if($operator == "*") $num_3 = $num_1 * $num_2;
     
-    if($ope == "+") $kazu_3 = $kazu_1 + $kazu_2;
-    if($ope == "*")$kazu_3 = $kazu_1 * $kazu_2;
-    if($ope == "-") $kazu_3 = $kazu_1 - $kazu_2;
-    if($ope == "/") $kazu_3 = $kazu_1 / $kazu_2;
-    
-  
-
+    }
     
     
     ?>
+<!DOCTYPE>
 <html>
 <head>
 <title>電卓</title>
@@ -58,18 +68,23 @@
 <!-- input 'text'と'submit'の二種 -->
 
 <p>
-<input type="text" name="kazu_1" value="<?php echo htmlspecialchars($kazu_1); ?>">
+<input type="text" name="num_1" value="<?php echo htmlspecialchars($num_1); ?>">
 
-    <select name= "operator">
-        <option>+</option>
-        <option>-</option>
-        <option>*</option>
-        <option>/</option>
-    </select>
+<select name= "operator">
+<?php ($_POST["operator"] =="+")?$val = "selected":$val = ""; ?>
+        <option value="+" <?=$val?>>+</option>
+<?php ($_POST["operator"] =="*")?$val = "selected":$val = ""; ?>
+        <option value="*" <?=$val?>>*</option>
+<?php ($_POST["operator"] =="-")?$val = "selected":$val = ""; ?>
+        <option value="-" <?=$val?>>-</option>
+<?php ($_POST["operator"] =="/")?$val = "selected":$val = ""; ?>
+        <option value="/" <?=$val?>>/</option>
+</select>
 
-<input type="text" name="kazu_2" value="<?php echo htmlspecialchars($kazu_2); ?>"> =
-<span><?php echo htmlspecialchars($kazu_3); ?></span>
+<input type="text" name="num_2" value="<?php echo htmlspecialchars($num_2); ?>"> =
+<span><?php echo htmlspecialchars($num_3); ?></span>
 <input type="submit" name="button_sum" value="計算">
+
 </p>
 </form>
 </body>
